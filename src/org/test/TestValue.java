4856fedfdfdbc7c472b41c13f88f004cb0f2081e
thivakar771 <thivakar771@gmail.com>
2021-11-25 17:39:48 +0530

    done

---
+++ src/org/test/TestValue.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class TestValue {
+public static void main(String[] args) {
+	
+	int a=20,b=10,c=30;
+	
+	
+	if (a+b>=c) {
+	
+		System.out.println("passed");
+	} else {
+       
+        System.out.println("failed");
+	}
+}
+}
